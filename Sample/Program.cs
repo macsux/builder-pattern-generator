991329f0a769b8dccc99516bc2349e858c3318cc
Andrew Stakhov <andrew.stakhov@gmail.com>
2020-05-01 14:12:02 -0400

    Fixup

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -5,24 +5,22 @@ using Newtonsoft.Json;
 
 namespace Sample
 {
-[GenerateBuilder]
-public partial class Person
-{
-    [Required]
-    public string FirstName { get; private set; }
-    [Required]
-    public string LastName { get; private set; }
-    public DateTime? BirthDate { get; private set; }
-}
+    [GenerateBuilder]
+    public partial class Person
+    {
+        [Required]
+        public string FirstName { get; private set; }
+        [Required]
+        public string LastName { get; private set; }
+        public DateTime? BirthDate { get; private set; }
+    }
     
     class Program
     {
         static void Main(string[] args)
         {
-            var myDataClass = Person.Builder 
-                .FirstName("John")
-                .LastName("Smith")
-                .BirthDate(new DateTime(1980,3,1))
+            var myDataClass = Person.Builder
+                .LastName("blah")
                 .Build();
             
             Console.WriteLine(JsonConvert.SerializeObject(myDataClass, Formatting.Indented));
